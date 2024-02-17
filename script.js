d83613fe49bfcdb772037ba928241d30bcec752b
Tchipen <153777126+Tchipencode@users.noreply.github.com>
2024-02-17 09:08:06 +0100

    game work with one color

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
    const gridContainer=document.querySelector(".container");
-   createGrid(16);
+  
    function createGrid(size){
       for (let i=0; i<size; i++){  
          for(let j=0;j<size; j++){
@@ -9,26 +9,41 @@
             gridItem.className="grid-item";
             gridItem.textContent=""
             gridContainer.appendChild(gridItem);  
+            gridItem.addEventListener("mousemove",()=>{
+               gridItem.style.backgroundColor="red";
+               //event.currentTarget.className("red");
+            });
+
+
          }
+
+         
       }
+
       
    }
-   
-   const gridCells=document.querySelectorAll(".grid-item");
+
+
+   /*const gridCells=document.querySelectorAll(".grid-item");
    gridCells.forEach(gridItem =>{
       gridItem.addEventListener("mousemove",()=>{
-         gridItem.style.backgroundColor="yellow";
+         gridItem.style.backgroundColor="red";
          //event.currentTarget.className("red");
       });
-   });
-   function changeColor(){
-      
+   });*/
+   
 
-   }
+  
+   function generateRandomColor() {
+      let h = Math.floor(Math.random() * 361);
+      let s = Math.floor(Math.random() * 101);
+      return `hsl(${h},${s}%)`;
+      
+    }
    
    function changeGrid(){
       let gridSize=+prompt("enter a new grid number between 1 to 100");
-      if(gridSize==0 || gridSize>=100 || isNaN(gridSize)){
+      if(gridSize<=0 || gridSize>100 || isNaN(gridSize)){
          alert("try a number between 1 to 100");
          
       }
@@ -46,16 +61,8 @@
          gridContainer.firstChild.remove();
       }  
    }
+   createGrid(16);
    
-
-
-   /*gridCells.forEach(gridItem =>{
-      gridItem.addEventListener("mouseout",()=>{
-         gridItem.style.backgroundColor="green";
-         //event.currentTarget.className("red");
-      });
-   });*/
- 
  
 
  
