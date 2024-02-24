bd4d5a9eb31fe8a71fb74ee3133e718db33fe4ad
Tchipen <153777126+Tchipencode@users.noreply.github.com>
2024-02-24 22:29:32 +0100

    game work v2

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 
 
    const gridContainer=document.querySelector(".container");
+   
   
    function createGrid(size){
-      for (let i=0; i<size; i++){  
-         for(let j=0;j<size; j++){
+      const square= size**2;
+      const gridSize= `${100 / size}%`;
+      //const containerStyle= getComputedStyle(gridContainer);
+      //const containerWidth= containerStyle.width;
+      //const containerHeight= containerStyle.height;
+      //const gridWidth= +containerWidth.split('px')[0] / size;
+      //const gridHeight= +containerHeight.split('px')[0] / size;
+      for (let i=0; i<square; i++){  
+      
             const gridItem=document.createElement("div");
             gridItem.className="grid-item";
-            gridItem.textContent=""
-            gridContainer.appendChild(gridItem);  
-            gridItem.addEventListener("mousemove",()=>{
-               gridItem.style.backgroundColor="red";
-               //event.currentTarget.className("red");
-            });
-
-
-         }
-
+            gridItem.textContent="";
+            gridContainer.appendChild(gridItem);
+            gridItem.style.width=gridSize;
+            gridItem.style.height=gridSize;
+            //gridItem.style.width=`${gridWidth}px`;
+            //gridItem.style.height=`${gridWidth}px`;
+               
          
-      }
-
-      
+            gridItem.addEventListener("mouseover",()=>{
+            let brightness = gridItem.dataset.brightness || 100;
+            gridItem.style.filter = `brightness(${brightness}%)`;
+            gridItem.dataset.brightness = Math.max(brightness - 10, 0);
+            });  
+            /*gridItem.addEventListener("mouseover",()=>{
+               gridItem.style.backgroundColor=generateRandomColor();
+               //event.currentTarget.className("red");
+            });*/
+              
+      }    
    }
 
 
@@ -35,9 +48,21 @@
 
   
    function generateRandomColor() {
-      let h = Math.floor(Math.random() * 361);
-      let s = Math.floor(Math.random() * 101);
-      return `hsl(${h},${s}%)`;
+      /*let currentBrightness = gridItem.dataset.brightness;
+    currentBrightness = parseInt(currentBrightness) - 10;
+
+    if (currentBrightness >= 0) {
+        gridItem.style.backgroundColor = `hsl(240, 100%,${currentBrightness}%)`
+        gridItem.dataset.brightness = currentBrightness;
+    }*/
+      let brightness = gridItem.dataset.brightness || 100;
+      gridItem.style.filter = `brightness(${brightness}%)`;
+      gridItem.dataset.brightness = Math.max(brightness - 10, 0);
+     
+      //return 'hsla(' + (Math.floor(Math.random()*360)) + ', 100%, 80%, 1)';
+      //let h = Math.floor(Math.random() * 360);
+      //let s = Math.floor(Math.random() * 100);
+      //return 'hsl(${h},${s}%,${luminosity}%)';
       
     }
    
@@ -61,8 +86,8 @@
          gridContainer.firstChild.remove();
       }  
    }
-   createGrid(16);
    
+   createGrid(16);
  
 
  
